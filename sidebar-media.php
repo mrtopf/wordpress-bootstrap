3a50900e54bf27da1f5e11f311e172f0c1f92f33
Christian Scholz <cs@comlounge.net>
2012-12-04 15:30:26 +0000

    fixed buttons and livestream area

--- sidebar-media.php
+++ sidebar-media.php
@@ -1,12 +1,12 @@
 <div id="sidebarmedia" class="fluid-sidebar sidebar span3" role="complementary">
-    <div style="margin-bottom: 20px;">
-        <iframe width="270" height="225" src="https://www.youtube-nocookie.com/embed/X_YRHHKOIhg?rel=0" frameborder="0" allowfullscreen></iframe>
-        <a class="btn btn-info btn-mini btn-block" href="/live">zum Livestream mit Chat</a>
-    </div>
+    <?php /*
     <a class="btn btn-primary btn-large btn-block" href="<?php echo esc_url(get_category_link( 22 )); ?>">Protokolle</a>
     <a class="btn btn-primary btn-large btn-block" href="<?php echo esc_url(get_category_link( 61 )); ?>">Video-Blog</a>
     <a class="btn btn-primary btn-large btn-block" href="<?php echo esc_url(get_category_link( 60 )); ?>">Podcast</a>
     <a class="btn btn-primary btn-large btn-block" href="<?php echo esc_url(get_category_link( 62 )); ?>">Bilder</a>
+    */
+    ?>
+
 
     <?php if ( is_active_sidebar( 'media' ) ) : ?>
 
