4004abf1c02528e30071f7337b419134f39502d6
Christian Scholz <cs@comlounge.net>
2012-12-04 15:29:31 +0000

    added slider template for nggallery, using foundation orbit

---
+++ nggallery/gallery-slider.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Template for Orbit slider Gallery + Captions
+ * **/
+?>
+<?php if (!defined ('ABSPATH')) die ('No direct access allowed'); ?><?php if (!empty ($gallery)) : ?>
+    <div class="slideshow">
+    <?php foreach ( $images as $image ) : ?>
+        <div data-caption="#htmlCaption-<?php echo $image->pid ?>">
+            <img title="<?php echo $image->alttext ?>" alt="<?php echo $image->alttext ?>" src="<?php echo $image->imageURL ?>" />
+        </div>
+    <?php endforeach; ?>
+    </div>
+    <?php foreach ( $images as $image ) : ?>
+        <div class="orbit-caption" id="htmlCaption-<?php echo $image->pid ?>">
+            <?php echo $image->alttext ?>
+        </div>
+    <?php endforeach; ?>
+<?php endif; ?>
+
