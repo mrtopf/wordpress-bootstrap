d73666e7f5796af7e4221b7a836acf7f2cca48e4
Christian Scholz <cs@comlounge.net>
2013-08-22 15:18:07 +0200

    removed double height attribute. WP seems not to clean that up.

--- sidebar.php
+++ sidebar.php
@@ -12,7 +12,7 @@
         <div id="featured">
             <?php while (have_posts()) : the_post(); ?>
                 <div data-caption="#caption-<?php the_ID();?>" >
-                    <a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail('wpbs-boxed', array('height' => '250', 'title' => get_the_title()))?></a>
+                    <a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail('wpbs-boxed', array('title' => get_the_title()))?></a>
                 </div>
             <?php endwhile; ?>
         </div>
